038c8573ac749331b02ad76813a629ea9c3063de
ArielLevy8 <ArielLevy1818@gmail.com>
2025-07-07 17:43:07 -0700

    app.py new bot 2.5 added!!

--- app.py
+++ app.py
@@ -94,6 +94,16 @@ def MAXChat(you):
     max_messages.append({"role": "assistant", "content": Chat_reply})
     return Chat_reply
 
+def MAX2Chat(you):
+    max_messages.append({"role": "user", "content": you})
+    response = client.chat.completions.create(
+        model ="gpt-4.5-preview",
+            messages =max_messages
+    )
+    Chat_reply = response.choices[0].message.content
+    max_messages.append({"role": "assistant", "content": Chat_reply})
+    return Chat_reply
+
 
 def generate_image(prompt: str) -> str:
     response = client.images.generate(
@@ -170,6 +180,14 @@ with gr.Blocks(theme = gr.themes.Soft(), title = "Talky.ai") as demo:
      chat_input = gr.Textbox(label = "Chat with Max.")
      chat_output = gr.Textbox(label = "Talky Max response.")
      chat_input.submit(fn = MAXChat, inputs = chat_input, outputs=chat_output)
+
+     new_chat_btn = gr.Button("Clear search")
+     new_chat_btn.click(lambda: ("", ""), None, [chat_input, chat_output])
+
+ with gr.Tab("MAX 2.5"):
+     chat_input = gr.Textbox(label = "Chat with MAX 2.5.")
+     chat_output = gr.Textbox(label = "MAX's response.")
+     chat_input.submit(fn = MAX2Chat, inputs = chat_input, outputs=chat_output)
 
      new_chat_btn = gr.Button("Clear search")
      new_chat_btn.click(lambda: ("", ""), None, [chat_input, chat_output])
